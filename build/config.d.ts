746dc19a90284962e307cbbacc2fe1b13c19f05b
Ryan Mehta <ryan.mehta@gmail.com>
2021-03-11 09:15:44 -0800

    add build

---
+++ build/config.d.ts
@@ -0,0 +1,3 @@
+import { ConnectConfig } from 'near-api-js';
+import { NearEnvironment } from './NearEnvironment';
+export declare function getConfig(env: NearEnvironment): ConnectConfig;
---
+++ build/index.d.ts
@@ -0,0 +1,4 @@
+import { NearProviderProps, NearProvider } from './NearProvider';
+export * from './NearEnvironment';
+export { NearProviderProps, NearProvider };
+export * from './hooks';
